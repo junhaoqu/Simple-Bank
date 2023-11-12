b66a64b3bdb171f49474232b94767d275d2b78c6
Junhao Qu <869142649qq@gmail.com>
2023-11-12 15:45:59 -0800

    add all sqlc

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -2,38 +2,28 @@ package db
 
 import (
 	"context"
-	"database/sql"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-type Store struct {
+// Store defines all functions to execute db queries and transactions
+type Store interface {
+	Querier
+	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+	CreateUserTx(ctx context.Context, arg CreateUserTxParams) (CreateUserTxResult, error)
+	VerifyEmailTx(ctx context.Context, arg VerifyEmailTxParams) (VerifyEmailTxResult, error)
+}
+
+// SQLStore provides all functions to execute SQL queries and transactions
+type SQLStore struct {
+	connPool *pgxpool.Pool
 	*Queries
-	db *sql.DB
 }
 
-func NewStore(db *sql.DB) *Store {
-	return &Store{
-		Queries: New(db),
-		db:      db,
+// NewStore creates a new store
+func NewStore(connPool *pgxpool.Pool) Store {
+	return &SQLStore{
+		connPool: connPool,
+		Queries:  New(connPool),
 	}
 }
-
-func (store *Store) exeecTx(ctx context.Context, fn func(*Queries) error) error {
-	tx, err := store.db.Begin(ctx, nil)
-	if err != nil {
-		return err
-	}
-
-	q := New(tx)
-	err = fn(q)
-	if err != nil {
-		if rbErr := tx.Rollback(); rbErr != nil {
-			return ftm.Errorf("tx err: %v, rb err: %v", err, rbErr)
-		}
-
-		return err
-	}
-
-	return tx.Commit()
-}
-
-func
