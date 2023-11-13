f54e908b8235a5b6eecce8d1664adfa2ca0e4ac0
Junhao Qu <869142649qq@gmail.com>
2023-11-12 17:39:09 -0800

    updated

--- util/random.go
+++ util/random.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 	"time"
@@ -12,6 +13,7 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
+// RandomInt generates a random integer between min and max
 func RandomInt(min, max int64) int64 {
 	return min + rand.Int63n(max-min+1)
 }
@@ -29,16 +31,24 @@ func RandomString(n int) string {
 	return sb.String()
 }
 
+// RandomOwner generates a random owner name
 func RandomOwner() string {
 	return RandomString(6)
 }
 
+// RandomMoney generates a random amount of money
 func RandomMoney() int64 {
 	return RandomInt(0, 1000)
 }
 
+// RandomCurrency generates a random currency code
 func RandomCurrency() string {
-	currencies := []string{"USD", "EUR", "CAD"}
+	currencies := []string{USD, EUR, CAD}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
+}
+
+// RandomEmail generates a random email
+func RandomEmail() string {
+	return fmt.Sprintf("%s@email.com", RandomString(6))
 }
